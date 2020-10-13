2b1cedb8908dee7a6d43a144e3145019641c8d87
0xC00005 <hecknet@163.com>
2020-10-13 09:14:56 -0700

    Add files via upload

---
+++ Assignment9a in IntelliJ/src/Assignment9a.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+/**
+ * @author m04.zhang
+ * @version 2020-10-05
+ * @project Assignment9a in IntelliJ
+ * @description
+ */
+public class Assignment9a {
+    private static final Integer carModelNeedFix[] = {119, 179, 189, 119, 179, 189, 780};
+    private int userCarModel;
+    private final Input input = new Input();
+
+    public Assignment9a() {
+        do {
+            System.out.print("Enter your model number (0 for done): ");
+            setUserCarModel(input.getIntWithErrorCheck());
+            if (Arrays.asList(carModelNeedFix).contains(getUserCarModel())) {
+                System.out.println("Your car is defective. Please have it fixed.");
+            } else {
+                System.out.println("Your car is OK.");
+            }
+        } while (getUserCarModel() != 0);
+        System.out.println("Program complete.");
+    }
+
+    public void setUserCarModel(int userCarModel) {
+        this.userCarModel = userCarModel;
+    }
+
+    public int getUserCarModel() {
+        return userCarModel;
+    }
+
+    public static void main(String[] args) {
+        Assignment9a testObject = new Assignment9a();
+    }
+}
